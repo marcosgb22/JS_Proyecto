7db9d0a4654d36a04d4c640014205b1afc41714a
Marcos Barrios <marcosgb22@gmail.com>
2022-08-09 00:09:30 -0300

    first commit

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,20 +1,50 @@
-let TOTAL = [0,0];
+let TOTAL = [0 , 0];
+let CRIPTO =  [0 , " "];
 
-function agregar_compra (NFT, VALOR){
 
+function elegir_cripto(MONEDA){
+  switch (MONEDA) {
+    case 'MONSTER':
+        VALOR = 1;
+        NOMBRE = "MONSTER";
+        alert('ELEGISTE MONSTER');
+        
+      break;
+    case 'AZURE':
+        VALOR = 0.0005;
+        NOMBRE = "AZURE";
+        alert('ELEGISTE AZURE');
+      break;
+    case 'PING':
+        VALOR = 0.1;
+        NOMBRE = "PING";
+        alert('ELEGISTE PING');
+      break;
+    default:
+    
+  }
+
+return [VALOR, NOMBRE];  
+}  
+
+
+
+
+
+function agregar_compra (NFT, VALOR, TOK){
 
   const item = {
     MEN: {
-      precio:1
+      precio: 1 * TOK
     },
     CALAVERA: {
-      precio:2
+      precio:2 * TOK
     },
     GIRASOL: {
-      precio:3
+      precio:3 * TOK
     },
     NICKITUS: {
-      precio:7
+      precio:7 * TOK
     }
   }
 
@@ -52,21 +82,25 @@ function agregar_compra (NFT, VALOR){
     return [VALOR, NO];    
 }
 
+var MONEDA=prompt('QUE CRIPTO QUIERES USAR? DSIPONIBLES = *MONSTER *AZURE *PING ');
+CRIPTO = elegir_cripto(MONEDA);
+
+
 var NFT=prompt('QUE NFT QUIERES COMPRAR? --ESCRIBE EL *NOMBRE PARA COMPRAR -- ESCRIBE *NO para SALIR ');
-TOTAL = agregar_compra(NFT, TOTAL[0]);
+TOTAL = agregar_compra(NFT, TOTAL[0],CRIPTO[0]);
 
 
 if (TOTAL[0] != 0){ 
     while (TOTAL[1]<1){
-    var NFT=prompt('LA SUMA DE LA COMPRA ES ' + TOTAL[0] + ' CRIPTOS DESEA AGREGAR OTRO? --ESCRIBE EL *NOMBRE PARA COMPRAR --*NO para SALIR');
-    TOTAL = agregar_compra(NFT, TOTAL[0]);
+    var NFT=prompt('LA SUMA DE LA COMPRA ES ' + TOTAL[0] + ' ' + CRIPTO[1] + ' DESEA AGREGAR OTRO? --ESCRIBE EL *NOMBRE PARA COMPRAR --*NO para SALIR');
+    TOTAL = agregar_compra(NFT, TOTAL[0],CRIPTO[0]);
 
     }
 }
 
 
 
-alert('COMPRA FINAL ES = ' + TOTAL[0] + ' CRIPTOS ');
+alert('COMPRA FINAL ES = ' + TOTAL[0] + ' ' + CRIPTO[1]);
 
 
 
