e688e3acc49eef254a20a5963f17fdeffeee348c
Marcos UTE <marcosgb22@gmail.com>
2022-08-24 17:07:26 -0300

    arreglo2

--- README.md
+++ README.md
@@ -1,20 +1,12 @@
 # JS_Simulador 2.0
 
-https://starlit-conkies-d00b4e.netlify.app/
+
 
 
 El sitio es de venta de NFT con criptomonedas
-*INGRESAR LOS DATOS EN MAYUSCULA*
 
-Deja elegir entre tres posibles critpomonedas
+Se agregaron js de eventos-
 
-MONSTER
-AZURE y 
-PING
+se agregaron botones de compra
 
-Despues de elegir la critpo eliges que NFT quieres comprar
-
-y al final devuelve el monto TOTAL en la critpomoneda elegida-
-
-
-Se agrego js que de estructura al DOM segun array de NFT. creando tarjetas de manera automatica
+se agrego json con info
--- js/view.js
+++ js/view.js
@@ -1,29 +1,14 @@
-//    Programa principal
 
-//    container.innerHTML = “
-//    
-//    <div class="card" style="width: 12rem;">
-//    <img src="Recursos/Imagenes/InShot_20220623_025042057.jpg" class="card-img-top" alt="...">
-//    <div class="card-body">
-//      <h5 class="card-title"></h5>
-//      <p class="card-text">NFT MEN</p>
-//      <a href="#" class="btn btn-primary">ADD TO CART</a>
-//    </div>
-//    </div>”
+import data from './nft.json' assert {type: 'json'};
 
 let Total = 0;
 
-
-const item =     [{  id: 1,  nombre: "MEN"      , precio: 1 , img: "Recursos/Imagenes/InShot_20220623_025042057.jpg" },
-                  {  id: 2,  nombre: "CALAVERA" , precio: 2 , img: "Recursos/Imagenes/tapa_eno_calavera.jpg" },
-                  {  id: 3,  nombre: "GIRASOL"  , precio: 3 , img: "Recursos/Imagenes/TapaEpEon.jpg"},
-                  {  id: 4,  nombre: "NICKITUS" , precio: 7 , img: "Recursos/Imagenes/nico.JPG"}];
+//localStorage.setItem("NFT", data);
+//
+//console.log (localStorage.getItem(NFT))
 
 
-
-for (const items of item) {
-
-//let contenedor = document.createElement("div");
+for (const items of data){
 
 let card = document.createElement("div");
 
@@ -41,7 +26,6 @@ card.innerHTML =
     document.getElementById("content_cards").appendChild(card);
 
 }
-
 
 let botonMEN = document.getElementById("MEN")
 let botonCALAVERA = document.getElementById("CALAVERA")
@@ -70,9 +54,6 @@ botonNICKITUS.onclick = () =>{
 }
 
 
-
-
-
 function cambiartotal(Total){
 
 let total = document.getElementById("content_total");
@@ -83,7 +64,5 @@ total.innerHTML =
     </div>
     <p class="color : white ">Total a pagar -  ${Total} ₿(MTR)</p>
     </div>`;
-
-//document.getElementById("content_total").append(total)
 
 }
